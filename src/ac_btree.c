ac000/libac

commit 9c97a4d2e6112c3ee22ff8960b55551233bffe4f

ac_btree: Adjust the ac_btree_foreach() interface
This is a function to iterate over the given tree applying the specified
action function on each node.

Underneath this uses the twalk(3) function which has the following
prototype

    void twalk(const void *root,
               void (*action)(const void *nodep, VISIT which,
                              int depth));

which doesn't allow to pass user data to the action() function, which
can often be useful without resorting to global variables.

However there is a twalk_r(3) function with the following prototype

    void twalk_r(const void *root,
                 void (*action)(const void *nodep, VISIT which,
                                void *closure),
                 void *closure);

which essentially replaces the depth argument with a user data one.

So we loose the depth argument to our action function but can now pass
user data into it if required which I think will be a lot more useful.
If we ever find we want/need the depth argument then we can perhaps do
something like this[0].

We also adjust some of the function parameters to match the above. i.e
mark the tree root as const and remove const from the VISIT which
argument, this is just a typedef to an enum.

We also update test.c to show the use of the 'which' variable in the
action() function.

Also make it clear that this is a 'binary search tree'.

[0]: https://www.gnu.org/software/libc/manual/html_node/Tree-Search-Function.html#index-twalk_005fr

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/ac_btree.c
@@ -1,9 +1,10 @@
 /* SPDX-License-Identifier: LGPL-2.1 */
 
 /*
- * ac_btree.c - Binary tree functions
+ * ac_btree.c - Binary search tree functions
  *
- * Copyright (c) 2017		Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (c) 2017, 2019 - 2020	Andrew Clayton
+ *					<andrew@digital-domain.net>
  */
 
 #define _GNU_SOURCE		/* tdestroy(3) */
@@ -62,12 +63,14 @@ void *ac_btree_new(int (*compar)(const void *, const void *),
  *
  * @tree: The binary tree to operate on
  * @action: Function to be called for each node
+ * @user_data: Optional user data argument passed to action() as closure
  */
-void ac_btree_foreach(ac_btree_t *tree, void (*action)(const void *nodep,
-						       const VISIT which,
-						       const int depth))
+void ac_btree_foreach(const ac_btree_t *tree,
+		      void (*action)(const void *nodep, VISIT which,
+				     void *data),
+		      void *user_data)
 {
-	twalk(tree->rootp, action);
+	twalk_r(tree->rootp, action, user_data);
 }
 
 /**

src/include/libac.h
@@ -207,10 +207,11 @@ typedef struct ac_slist {
 #pragma GCC visibility push(default)
 extern void *ac_btree_new(int (*compar)(const void *, const void *),
 			  void (*free_node)(void *nodep));
-extern void ac_btree_foreach(ac_btree_t *tree,
+extern void ac_btree_foreach(const ac_btree_t *tree,
 			     void (*action)(const void *nodep,
-					    const VISIT which,
-					    const int depth));
+					    VISIT which,
+					    void *data),
+			     void *user_data);
 extern void *ac_btree_lookup(const ac_btree_t *tree, const void *key);
 extern void *ac_btree_add(ac_btree_t *tree, const void *key);
 extern void *ac_btree_remove(ac_btree_t *tree, const void *key);

src/test.c
@@ -3,7 +3,7 @@
 /*
  * test.c - Test harness for libac
  *
- * Copyright (c) 2017 - 2018	Andrew Clayton <andrew@digital-domain.net>
+ * Copyright (c) 2017 - 2020	Andrew Clayton <andrew@digital-domain.net>
  */
 
 #define _GNU_SOURCE			/* strdup(3), struct addrinfo */
@@ -34,12 +34,25 @@ static void free_tnode(void *data)
 	free(tn);
 }
 
-static void print_node(const void *data, const VISIT which __always_unused,
-		       const int depth __always_unused)
+static void print_node(const void *data, VISIT which,
+		       void *user_data __always_unused)
 {
 	struct tnode *tn = *(struct tnode **)data;
 
-	printf("%d : %s\n", tn->key, (char *)tn->data);
+	/*
+	 * See the tsearch(3) man page for an explanation of the below
+	 * values.
+	 *
+	 * Here, we only want to print each node once.
+	 */
+	switch (which) {
+	case preorder:
+	case endorder:
+		return;
+	case postorder:
+	case leaf:
+		printf("%d : %s\n", tn->key, (char *)tn->data);
+	}
 }
 
 static int compare(const void *pa, const void *pb)
@@ -86,7 +99,7 @@ static void btree_test(void)
 	tn = ac_btree_lookup(tree, &stn);
 	printf("Found tnode: %d - %s\n", tn->key, (char *)tn->data);
 
-	ac_btree_foreach(tree, print_node);
+	ac_btree_foreach(tree, print_node, NULL);
 
 	ac_btree_remove(tree, &stn);
 	ac_btree_destroy(tree);

commit abeae35baf6093bdf880dd5b564b3a7841b7318d

ac_btree: Re-work ac_btree_foreach() (again...)
Commit 9c97a4d ("ac_btree: Adjust the ac_btree_foreach() interface")
unfortunately broke compilation under glibc < 2.30.

ac_btree.c: In function 'ac_btree_foreach':
ac_btree.c:73:2: warning: implicit declaration of function 'twalk_r' [-Wimplicit-function-declaration]
  twalk_r(tree->rootp, action, user_data);
  ^

That was under CentOS 7 with glibc 2.17.

I had missed the fact that the twalk_r(3) function was only introduced
in glibc 2.30 (2019-08-01).

However, instead of simply reverting the above commit, I've put back the
original version of ac_btree_foreach() and renamed the new version that
uses twalk_r() to ac_btree_foreach_data() and guarded it by a glibc
version check.

Fixes: 9c97a4d ("ac_btree: Adjust the ac_btree_foreach() interface")
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/ac_btree.c
@@ -63,15 +63,31 @@ void *ac_btree_new(int (*compar)(const void *, const void *),
  *
  * @tree: The binary tree to operate on
  * @action: Function to be called for each node
- * @user_data: Optional user data argument passed to action() as closure
  */
 void ac_btree_foreach(const ac_btree_t *tree,
 		      void (*action)(const void *nodep, VISIT which,
-				     void *data),
-		      void *user_data)
+				     int depth))
+{
+	twalk(tree->rootp, action);
+}
+
+/* twalk_r(3) was introduced in glibc 2.30 */
+#if __GLIBC__ >= 2 && __GLIBC_MINOR__ >= 30
+/**
+ * ac_btree_foreach_data - iterate over the tree with user_data
+ *
+ * @tree: The binary tree to operate on
+ * @action: Function to be called for each node
+ * @user_data: Optional user data argument passed to action() as closure
+ */
+void ac_btree_foreach_data(const ac_btree_t *tree,
+			   void (*action)(const void *nodep, VISIT which,
+					  void *data),
+			   void *user_data)
 {
 	twalk_r(tree->rootp, action, user_data);
 }
+#endif
 
 /**
  * ac_btree_lookup - lookup a node in the tree

src/include/libac.h
@@ -208,10 +208,14 @@ typedef struct ac_slist {
 extern void *ac_btree_new(int (*compar)(const void *, const void *),
 			  void (*free_node)(void *nodep));
 extern void ac_btree_foreach(const ac_btree_t *tree,
-			     void (*action)(const void *nodep,
-					    VISIT which,
-					    void *data),
-			     void *user_data);
+			     void (*action)(const void *nodep, VISIT which,
+					    int depth));
+#if __GLIBC__ >= 2 && __GLIBC_MINOR__ >= 30
+extern void ac_btree_foreach_data(const ac_btree_t *tree,
+				  void (*action)(const void *nodep,
+						 VISIT which, void *data),
+				  void *user_data);
+#endif
 extern void *ac_btree_lookup(const ac_btree_t *tree, const void *key);
 extern void *ac_btree_add(ac_btree_t *tree, const void *key);
 extern void *ac_btree_remove(ac_btree_t *tree, const void *key);

src/test.c
@@ -35,7 +35,7 @@ static void free_tnode(void *data)
 }
 
 static void print_node(const void *data, VISIT which,
-		       void *user_data __always_unused)
+		       int depth __always_unused)
 {
 	struct tnode *tn = *(struct tnode **)data;
 
@@ -99,7 +99,7 @@ static void btree_test(void)
 	tn = ac_btree_lookup(tree, &stn);
 	printf("Found tnode: %d - %s\n", tn->key, (char *)tn->data);
 
-	ac_btree_foreach(tree, print_node, NULL);
+	ac_btree_foreach(tree, print_node);
 
 	ac_btree_remove(tree, &stn);
 	ac_btree_destroy(tree);