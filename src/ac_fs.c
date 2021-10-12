ac000/libac

commit d28cfa7d5d16f7215ddf6b0840ac81148d5b47ce

ac_fs: Add a file copy function
Add ac_fs_copy(), that takes three arguments; the file to copy, the
destination and flags which can either be 0 or AC_FS_COPY_OVERWRITE to
allow the destination to be overwritten.

This is implemented using sendfile(2) and should work fine if your using
a 2.4 or >= 2.6.33 kernel.

See the fs_test() function in test.c for example usage.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/ac_fs.c
@@ -12,9 +12,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <sys/stat.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
 #include <errno.h>
+
+#include "include/libac.h"
 
 /**
  * ac_fs_is_posix_name - checks if a filename follows POSIX guidelines
@@ -94,4 +99,49 @@ int ac_fs_mkdir_p(const char *path)
 	free(ptr);
 
 	return ret;
+}
+
+#define IO_SIZE	(1024*1024 * 2)
+/**
+ * ac_fs_copy - copy a file
+ *
+ * @from: The source
+ * @to: The destination
+ * @flags: Can be 0 or AC_FS_COPY_OVERWRITE (to overwrite the destination)
+ *
+ * This is implemented using sendfile(2) and should work fine if your using
+ * a 2.4 or >= 2.6.33 kernel.
+ *
+ * Returns:
+ *
+ * 0 on success, -1 otherwise, check errno
+ */
+ssize_t ac_fs_copy(const char *from, const char *to, int flags)
+{
+	int ifd;
+	int ofd;
+	int oflags = O_EXCL;
+	ssize_t bytes_wrote;
+
+	ifd = open(from, O_RDONLY | O_CLOEXEC);
+	if (ifd == -1)
+		return -1;
+
+	if (flags & AC_FS_COPY_OVERWRITE) {
+		oflags &= ~(O_EXCL);
+		oflags |= O_TRUNC;
+	}
+
+	ofd = open(to, O_WRONLY | O_CREAT | O_CLOEXEC | oflags, 0666);
+	if (ofd == -1)
+		return -1;
+
+	do {
+		bytes_wrote = sendfile(ofd, ifd, NULL, IO_SIZE);
+	} while (bytes_wrote > 0);
+
+	close(ifd);
+	close(ofd);
+
+	return bytes_wrote;
 }

src/include/libac.h
@@ -33,6 +33,8 @@ typedef int16_t  s16;
 typedef uint8_t   u8;
 typedef int8_t    s8;
 
+#define AC_FS_COPY_OVERWRITE	0x01
+
 typedef enum ac_si_units_t {
 	AC_SI_UNITS_NO = 0,
 	AC_SI_UNITS_YES
@@ -110,6 +112,7 @@ void ac_cqueue_destroy(ac_cqueue_t *cqueue);
 
 bool ac_fs_is_posix_name(const char *name);
 int ac_fs_mkdir_p(const char *path);
+ssize_t ac_fs_copy(const char *from, const char *to, int flags);
 
 void ac_misc_ppb(u64 bytes, ac_si_units_t si, ac_misc_ppb_t *ppb);
 

src/test.c
@@ -15,6 +15,7 @@
 #include <string.h>
 #include <search.h>
 #include <netdb.h>
+#include <unistd.h>
 #include <time.h>
 
 #include "include/libac.h"
@@ -170,6 +171,7 @@ static void cqueue_test(void)
 
 static void fs_test(void)
 {
+	int ret;
 	const char name1[] = "-bad name";
 	const char name2[] = "a_good_name";
 
@@ -189,6 +191,16 @@ static void fs_test(void)
 
 	printf("Creating direstory : /tmp/libac/mkdir_p/test\n");
 	ac_fs_mkdir_p("/tmp/libac/mkdir_p/test");
+
+	unlink("/tmp/services");
+	printf("Copying /etc/services -> /tmp/services\n");
+	ac_fs_copy("/etc/services", "/tmp/services", 0);
+	printf("Copying /etc/services -> /tmp/services (overwrite)\n");
+	ac_fs_copy("/etc/services", "/tmp/services", AC_FS_COPY_OVERWRITE);
+	printf("Copying /etc/services -> /tmp/services\n");
+	ret = ac_fs_copy("/etc/services", "/tmp/services", 0);
+	if (ret == -1)
+		perror("ac_fs_copy");
 
 	printf("*** %s\n\n", __func__);
 }

commit 9f73da49a61fe5512acbe8ef7d8996b0452038eb

ac_fs: Fix return value of ac_fs_copy()
The intent was to return either -1 on error or the number of bytes
written.

However we only tracked the current number of bytes written not the
overall total.

Fixes: d28cfa7 ("ac_fs: Add a file copy function")
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/ac_fs.c
@@ -128,6 +128,7 @@ ssize_t ac_fs_copy(const char *from, const char *to, int flags)
 	int ret;
 	int oflags = O_EXCL;
 	unsigned window = 0;
+	ssize_t total = 0;
 	ssize_t bytes_wrote = -1;
 	struct stat sb;
 
@@ -165,6 +166,9 @@ ssize_t ac_fs_copy(const char *from, const char *to, int flags)
 
 	do {
 		bytes_wrote = sendfile(ofd, ifd, NULL, IO_SIZE);
+		if (bytes_wrote == -1)
+			break;
+		total += bytes_wrote;
 
 		/*
 		 * Try not to blow the page cache. After each sendfile() we
@@ -196,5 +200,5 @@ cleanup:
 	close(ifd);
 	close(ofd);
 
-	return bytes_wrote;
+	return bytes_wrote == -1 ? bytes_wrote : total;
 }