ac000/libac

commit b8ebfc15f01988f1233909129eb45adb1968994c

ac_fs/ac_fs_copy: Some minor code refactor
Refactor the checking of 'window' to make the code flow better (from a
readability standpoint at least).

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/ac_fs.c
@@ -167,17 +167,19 @@ ssize_t ac_fs_copy(const char *from, const char *to, int flags)
 		 */
 		sync_file_range(ofd, window * IO_SIZE, IO_SIZE,
 				SYNC_FILE_RANGE_WRITE);
-		if (window) {
-			sync_file_range(ofd, (window-1) * IO_SIZE, IO_SIZE,
-					SYNC_FILE_RANGE_WAIT_BEFORE |
-					SYNC_FILE_RANGE_WRITE |
-					SYNC_FILE_RANGE_WAIT_AFTER);
-			posix_fadvise(ofd, (window-1) * IO_SIZE, IO_SIZE,
-					POSIX_FADV_DONTNEED);
-			posix_fadvise(ifd, (window-1) * IO_SIZE, IO_SIZE,
-					POSIX_FADV_DONTNEED);
-		}
+
+		if (window == 0)
+			continue;
+
 		window++;
+		sync_file_range(ofd, (window-1) * IO_SIZE, IO_SIZE,
+				SYNC_FILE_RANGE_WAIT_BEFORE |
+				SYNC_FILE_RANGE_WRITE |
+				SYNC_FILE_RANGE_WAIT_AFTER);
+		posix_fadvise(ofd, (window-1) * IO_SIZE, IO_SIZE,
+			      POSIX_FADV_DONTNEED);
+		posix_fadvise(ifd, (window-1) * IO_SIZE, IO_SIZE,
+			      POSIX_FADV_DONTNEED);
 	} while (bytes_wrote > 0);
 
 cleanup:

commit cbfdb4b222369a36d9c851bc60344fa82f87fee7

ac_fs: Fix ac_fs_copy()
Unfortunately, commit b8ebfc1 ("ac_fs/ac_fs_copy: Some minor code
refactor") caused some change in behaviour. We would only ever call the
first sync_file_range(2) and not the second or any of the
posix_fadvise(2) calls. It probably still worked, just not how it's
supposed to.

Due to the

  if (window == 0)
      continue;

check we never increment window and always short circuit round the loop.

Rather than continuing create a label at the end of the loop, increment
the window there and jump to it the first time round the loop.

We do this rather than revert the broken commit so as to keep the intent
of that change.

Fixes: b8ebfc1 ("ac_fs/ac_fs_copy: Some minor code refactor")
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/ac_fs.c
@@ -177,9 +177,8 @@ ssize_t ac_fs_copy(const char *from, const char *to, int flags)
 				SYNC_FILE_RANGE_WRITE);
 
 		if (window == 0)
-			continue;
+			goto incr_win;
 
-		window++;
 		sync_file_range(ofd, (window-1) * IO_SIZE, IO_SIZE,
 				SYNC_FILE_RANGE_WAIT_BEFORE |
 				SYNC_FILE_RANGE_WRITE |
@@ -188,6 +187,9 @@ ssize_t ac_fs_copy(const char *from, const char *to, int flags)
 			      POSIX_FADV_DONTNEED);
 		posix_fadvise(ifd, (window-1) * IO_SIZE, IO_SIZE,
 			      POSIX_FADV_DONTNEED);
+
+incr_win:
+		window++;
 	} while (bytes_wrote > 0);
 
 cleanup: