ac000/libac

commit 5cf9e5683f928e68eef54b66f35037c7b9b93119

ac_fs: Fix potential issue in ac_fs_mkdir_p()
GitHub code scanning picked up a potential problem in ac_fs_mkdir_p()
where we could overwrite the last '\0' in mdir[] with a '/'.

We need to make mdir 1 bigger to allow for the fact that we add a
trailing '/' to the end of the directory path each time around the loop,
ready for the next directory component to get added.

Switch to using PATH_MAX for the buffer size and add 1 to it, we can
then just against PATH_MAX for the path length rather than the size of
the mdir buffer.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/ac_fs.c
@@ -16,6 +16,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <errno.h>
 
 #include "include/libac.h"
@@ -73,15 +74,15 @@ int ac_fs_mkdir_p(int dirfd, const char *path, mode_t mode)
 {
 	char *dir;
 	char *ptr;
-	char mdir[4096] = "\0";
+	char mdir[PATH_MAX + 1] = "\0";	/* +1 for a trailing '/' */
 	int ret = 0;
 
-	if (strlen(path) >= sizeof(mdir)) {
+	if (strlen(path) >= PATH_MAX) {
 		errno = ENAMETOOLONG;
 		return -1;
 	}
 
-	if (*path == '/')
+	if (*path != '/')
 		strcat(mdir, "/");
 
 	dir = strdup(path);

commit a50d4e3cab7e174b4a0a643976b7e2d752ceac8b

ac_fs: Fix ac_fs_mkdir_p()
This was broken in a couple of ways

1) An absolute path would result in mkdirat(2) trying to create a
   directory with an empty name "".
2) Relative pathnames would be converted into absolute paths.

Fixes: 5cf9e56 ("ac_fs: Fix potential issue in ac_fs_mkdir_p()")
Fixes: 0cee4d9 ("ac_fs: Re-work ac_fs_mkdir_p()")
Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

src/ac_fs.c
@@ -82,9 +82,6 @@ int ac_fs_mkdir_p(int dirfd, const char *path, mode_t mode)
 		return -1;
 	}
 
-	if (*path != '/')
-		strcat(mdir, "/");
-
 	dir = strdup(path);
 	ptr = dir;
 	for (;;) {
@@ -94,13 +91,18 @@ int ac_fs_mkdir_p(int dirfd, const char *path, mode_t mode)
 		if (!token)
 			break;
 
+		if (*token == '\0' && *path == '/')
+			strcat(mdir, "/");
+
 		strcat(mdir, token);
 		ret = mkdirat(dirfd, mdir, mode);
 		if (ret == -1 && errno != EEXIST) {
 			ret = -1;
 			break;
 		}
-		strcat(mdir, "/");
+
+		if (*token != '\0')
+			strcat(mdir, "/");
 	}
 	free(dir);
 